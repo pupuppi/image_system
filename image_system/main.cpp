3481a6b432f39481affc895da64315775113d3ed
m_ogawa <pazudaruma@gmail.com>
2017-06-16 18:19:15 +0900

    project file

--- image_system/main.cpp
+++ image_system/main.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
+#include<opencv2\opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
 auto main() -> int {
-    cout << "hello" << endl;
+    
+    VideoCapture cap(0);
+
+    Mat frame;
+    while (true) {
+        cap >> frame;
+
+        imshow("window", frame);
+        int key = waitKey(2);
+        if (key == 27) {
+            break;
+        }
+    }
+
     return 0;
 }
