52393f1402d92e1673264b116004fa5b61eeaf34
m_ogawa <pazudaruma@gmail.com>
2017-06-29 15:58:06 +0900

    analyzer 追加

---
+++ image_system/frame_analyzer.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include<opencv2\opencv.hpp>
+
+
+class FrameAnalyzer {
+private:
+    cv::CascadeClassifier cc;
+    cv::Ptr<cv::BackgroundSubtractorMOG2> bgs;
+
+public:
+    struct Result {
+        double skinRate;
+
+    };
+
+    FrameAnalyzer() {
+        constexpr char* cascadePath = "haarcascade_frontalface_default.xml";
+        if (!cc.load(cascadePath)) {
+            exit(2);
+        }
+
+        bgs = cv::createBackgroundSubtractorMOG2();
+    }
+
+    Result analyze(const cv::Mat& frame) {
+        using namespace cv;
+        using namespace std;
+
+        Mat target;
+        resize(frame, target, Size(640, 480));
+        medianBlur(target, target, 5);
+
+        Mat gray;
+        cvtColor(target, gray, COLOR_BGR2GRAY);
+        equalizeHist(gray, gray);
+
+        vector<Rect> faces;
+        cc.detectMultiScale(gray, faces, 1.1, 3, 0, Size(30, 30));
+        for (auto& face : faces) {
+            const Point c(face.x + face.width / 2, face.y + face.height);
+            ellipse(target, c, Size(1.1*face.width / 2, 1.1*face.height), 0, 0, 360, Scalar(0, 0, 0), -1);
+        }
+
+        Mat fore, masked;
+        bgs->apply(target, fore);
+        target.copyTo(masked, fore);
+
+        cvtColor(masked, masked, COLOR_BGR2HSV);
+        Mat skin;
+        inRange(masked, Scalar(0, 30, 40), Scalar(40, 240, 240), skin);
+
+        Result result;
+
+        // skin ‚ð‚¢‚¶‚ê‚Î‚¢‚¢Š´‚¶‚É‚È‚é‚æ
+        result.skinRate = mean(skin)[0];
+
+        imshow("skin", skin);
+
+        return result;
+    }
+
+};
--- image_system/main.cpp
+++ image_system/main.cpp
@@ -1,16 +1,26 @@
 #include<iostream>
 #include<opencv2\opencv.hpp>
 
+#include"frame_analyzer.h"
+
 using namespace std;
 using namespace cv;
 
 auto main() -> int {
     
     VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        exit(1);
+    }
+
+    FrameAnalyzer fa;
 
     Mat frame;
     while (true) {
         cap >> frame;
+
+        auto result = fa.analyze(frame);
+        cout << result.skinRate << endl;
 
         imshow("window", frame);
         int key = waitKey(2);
